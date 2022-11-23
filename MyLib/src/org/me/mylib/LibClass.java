2d8667cc7410deb6a8a7a4852fff8674bd3206bc
OxY623 <fuzzydoorproductions0@gmail.com>
2022-11-23 19:44:31 +0300

    Summary

---
+++ MyLib/src/org/me/mylib/LibClass.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.me.mylib;
+
+/**
+ *
+ * @author 37533
+ */
+public class LibClass {
+
+    public static String acrostic(String[] args) {
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].length() > i) {
+                b.append(args[i].charAt(i));
+            } else {
+                b.append('?');
+            }
+        }
+        return b.toString();
+    }
+}
